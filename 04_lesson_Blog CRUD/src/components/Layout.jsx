6b4d4e665dadb9b3001f3fa3ed876ae39e8f7d88
rishishal <rishisahilcomputer@gmail.com>
2023-10-04 13:16:03 +0530

    lesson 4 completed

---
+++ 04_lesson_Blog CRUD/src/components/Layout.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Header from "./Header";
+const Layout = () => {
+  return (
+    <>
+      <Header />
+      <main className='App'>
+        <Outlet />
+      </main>
+    </>
+  );
+};
+export default Layout;
